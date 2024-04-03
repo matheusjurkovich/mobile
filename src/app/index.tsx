1d7275d64bf3a5b1a5595f20a1c0cd8a8f995233
matheusjurkovich <matheusjurkovich@gmail.com>
2024-04-02 22:52:06 -0300

    Intial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,29 @@
+import { View, Image } from "react-native";
+import { Input } from "@/components/Input";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { colors } from "@/styles/colors";
+
+export default function Home() {
+  return (
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <Image
+        source={require("@/assets/logo.png")}
+        className="h-16"
+        resizeMode="contain"
+      />
+      <View className="w-full mt-12 gap-3">
+        <Input>
+          <MaterialCommunityIcons
+            name="ticket-confirmation-outline"
+            size={20}
+            color={colors.green[200]}
+          />
+          <Input.Field placeholder="Código do ingresso" />
+        </Input>
+      </View>
+    </View>
+  );
+}
+
+
+//1:31h
